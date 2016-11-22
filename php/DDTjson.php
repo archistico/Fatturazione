96cb503c1e48d266b08d3d112719336a4e6ef272
Emilie Rollandin <info@archistico.com>
2016-11-22 20:54:18 +0100

    Inserimento in fattura di DDT dinamico

---
+++ php/DDTjson.php
@@ -0,0 +1,34 @@
+<?php
+
+try {
+    include 'config.php';
+
+    $cliente_id = $_POST['cliente_id'];
+
+    $db = new PDO("mysql:host=" . $dbhost . ";dbname=" . $dbname, $dbuser, $dbpswd);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    $db->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES UTF8');
+
+    $sql = "SELECT ddt.*, cliente.* ".
+        "FROM ddt INNER JOIN cliente ON ddt.ddt_fkcliente = cliente.cli_id ".
+        "WHERE ddt.ddt_annullato = 0 AND ddt.ddt_fkfattura IS NULL AND ddt.ddt_fatturazioneelettronica = 0 AND ddt.ddt_fkcliente = ".$cliente_id;
+
+    $result = $db->query($sql);
+
+    foreach ($result as $row) {
+        $row = get_object_vars($row);
+        $numero_padded = sprintf("%04d", $row['ddt_numero']);
+        $dataEmissione = DateTime::createFromFormat('Y-m-d', $row['ddt_data'])->format('d/m/Y');
+
+        $listaDDT[] = array('ddt_id' => $row['ddt_id'], 'descrizione' => "DDT ".$numero_padded . " del " .$dataEmissione. " - &euro; " . $row['ddt_importo']);
+    }
+    // chiude il database
+    $db = NULL;
+
+    header('Content-type:application/json;charset=utf-8');
+    echo json_encode($listaDDT);
+
+} catch (PDOException $e) {
+    throw new PDOException("Error  : " . $e->getMessage());
+}
